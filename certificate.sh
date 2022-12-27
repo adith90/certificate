2efed84aa5d3e1abe80545d91ac58fc5ebb49440
adith90 <69346854+adith90@users.noreply.github.com>
2022-12-27 19:51:47 +0530

    Update certificate.sh

--- certificate.sh
+++ certificate.sh
@@ -3,7 +3,7 @@ date=$(date +%s)
 #find /etc/kubernetes/pki/ -type f -name "*.crt" -print|egrep -v 'ca.crt$'|xargs -L 1 -t  -i bash -c 'openssl x509  -noout -text -in {}|grep After' >cert.txt
 #echo "ignore above output"
 #expiry=`cat cert.txt | awk -F " " 'NR==1{print $4,$5,$6,$7,$8}'`
-expiry=$(date -d "2023-01-26" +%s)
+expiry=$(date -d "2023-01-01" +%s)
 diff=`expr $expiry - $date`
 days_remain=`expr $diff / 86400`
 if [ $diff -lt 1296000 ]; then

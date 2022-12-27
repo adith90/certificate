b53c3032855007803d49cdbb5729ced1a3557bc8
adith90 <69346854+adith90@users.noreply.github.com>
2022-12-27 19:46:23 +0530

    Create certificate.sh

---
+++ certificate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+date=$(date +%s)
+#find /etc/kubernetes/pki/ -type f -name "*.crt" -print|egrep -v 'ca.crt$'|xargs -L 1 -t  -i bash -c 'openssl x509  -noout -text -in {}|grep After' >cert.txt
+#echo "ignore above output"
+#expiry=`cat cert.txt | awk -F " " 'NR==1{print $4,$5,$6,$7,$8}'`
+expiry=$(date -d "2023-01-26" +%s)
+diff=`expr $expiry - $date`
+days_remain=`expr $diff / 86400`
+if [ $diff -lt 1296000 ]; then
+   echo "certificate dew for renewal in $days_remain"
+   echo "Need to renew certificate"
+else
+   echo "certification is valid"
+   echo "certificate remaining days= $days_remain"
+fi
